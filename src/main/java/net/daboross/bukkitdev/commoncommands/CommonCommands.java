38d1d378b8108613deca87b512cd1690864fdf2a
Dabo Ross <dabodialga@gmail.com>
2013-08-04 01:56:40 -0700

    Whatever

--- src/main/java/net/daboross/bukkitdev/commoncommands/CommonCommands.java
+++ src/main/java/net/daboross/bukkitdev/commoncommands/CommonCommands.java
@@ -16,14 +16,9 @@
  */
 package net.daboross.bukkitdev.commoncommands;
 
-/**
- *
- * @author daboross
- */
 import java.io.IOException;
 import java.util.logging.Level;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -60,31 +55,17 @@ public final class CommonCommands extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("list")) {
-            Player[] onlinePlayers = Bukkit.getServer().getOnlinePlayers();
-            if (onlinePlayers.length == 0) {
-                sender.sendMessage(ColorList.REG + "There are no players online.");
-            } else if (onlinePlayers.length == 1) {
-                sender.sendMessage(ColorList.REG + "There is one player online:");
-                sender.sendMessage(ColorList.NAME + onlinePlayers[0].getDisplayName());
-            } else {
-                sender.sendMessage(ColorList.TOP + "There are " + ColorList.DATA + onlinePlayers.length + ColorList.TOP + " players online:");
-                StringBuilder messageBuilder = new StringBuilder(ColorList.NAME).append(onlinePlayers[0].getDisplayName());
-                for (int i = 1; i < onlinePlayers.length; i++) {
-                    messageBuilder.append(ColorList.DIVIDER).append(", ").append(ColorList.NAME).append(onlinePlayers[i].getDisplayName());
-                }
-                sender.sendMessage(messageBuilder.toString());
-            }
-        } else if (cmd.getName().equalsIgnoreCase("w")) {
+        if (cmd.getName().equalsIgnoreCase("w")) {
             if (!(sender instanceof Player)) {
                 sender.sendMessage(ColorList.ERR + "Must be run by a player");
                 return true;
             }
+
             Player p = (Player) sender;
             Location loc = p.getLocation();
-            sender.sendMessage(ColorList.DATA + (int) loc.getX() + ColorList.REG + ", "
-                    + ColorList.DATA + (int) loc.getY() + ColorList.REG + ", "
-                    + ColorList.DATA + (int) loc.getZ() + ColorList.REG + ", "
+            sender.sendMessage((args.length == 0 ? "" : ColorList.REG + "Server:" + ColorList.DATA + args[0] + ColorList.REG + ", ") + ColorList.DATA + loc.getBlockX() + ColorList.REG + ", "
+                    + ColorList.DATA + loc.getBlockY() + ColorList.REG + ", "
+                    + ColorList.DATA + loc.getBlockZ() + ColorList.REG + ", "
                     + ColorList.DATA + loc.getWorld().getName());
         } else if (cmd.getName().equalsIgnoreCase("wi")) {
             if (args.length == 0) {
@@ -92,14 +73,7 @@ public final class CommonCommands extends JavaPlugin {
                 sender.sendMessage(ColorList.REG + "Usage: " + ColorList.CMD + "/" + label + ColorList.ARGS_SURROUNDER + " <" + ColorList.ARGS + "Player" + ColorList.ARGS_SURROUNDER + ">");
                 return true;
             }
-            Player player = null;
-            String lowerCaseArg = args[0].toLowerCase();
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                if (p.getName().toLowerCase().contains(lowerCaseArg) || ChatColor.stripColor(p.getDisplayName()).toLowerCase().contains(lowerCaseArg)) {
-                    player = p;
-                    break;
-                }
-            }
+            Player player = Bukkit.getPlayerExact(args[0]);
             if (player == null) {
                 sender.sendMessage(ColorList.ERR + "Player " + ColorList.ERR_ARGS + args[0] + ColorList.ERR + " not found");
                 sender.sendMessage(ColorList.REG + "Usage: " + ColorList.CMD + "/" + label + ColorList.ARGS_SURROUNDER + " <" + ColorList.ARGS + "Player" + ColorList.ARGS_SURROUNDER + ">");
