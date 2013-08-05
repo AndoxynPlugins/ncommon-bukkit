0ba6f9edf38710dc15f6cf4ee03ae7930f6e083c
Dabo Ross <daboross@daboross.net>
2013-08-05 12:13:08 -0700

    Things

--- src/main/java/net/daboross/bukkitdev/commoncommands/PlayerListener.java
+++ src/main/java/net/daboross/bukkitdev/commoncommands/PlayerListener.java
@@ -4,6 +4,8 @@
 package net.daboross.bukkitdev.commoncommands;
 
 import java.util.logging.Level;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -14,22 +16,22 @@ import org.bukkit.event.player.PlayerQuitEvent;
  * @author daboross
  */
 public class PlayerListener implements Listener {
-    
+
     private final CommonCommands plugin;
-    
+
     public PlayerListener(CommonCommands plugin) {
         this.plugin = plugin;
     }
-    
+
     @EventHandler
     public void onJoin(PlayerJoinEvent evt) {
         evt.setJoinMessage(null);
-        plugin.getLogger().log(Level.INFO, "{0} joined this server.", evt.getPlayer().getName());
+        Bukkit.getServer().getLogger().log(Level.INFO, ChatColor.YELLOW + "{0} joined this server.", evt.getPlayer().getName());
     }
-    
+
     @EventHandler
     public void onQuit(PlayerQuitEvent evt) {
         evt.setQuitMessage(null);
-        plugin.getLogger().log(Level.INFO, "{0} left this server.", evt.getPlayer().getName());
+        Bukkit.getServer().getLogger().log(Level.INFO, ChatColor.YELLOW + "{0} left this server.", evt.getPlayer().getName());
     }
 }
